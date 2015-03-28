220bae0e753724423b07d2bed5478ea9a28e95aa
neilinglese <neilinglese@gmail.com>
2015-03-28 13:18:59 -0500

    fixed updating email for account

--- client/templates/profile/profile.js
+++ client/templates/profile/profile.js
@@ -9,6 +9,7 @@ Template.profile.events({
         var username = event.target.userName.value;
         var lastname = event.target.lastName.value;
         var email = event.target.Email.value;
+        Meteor.call('updateemailfunction', email);
         Meteor.users.update({_id:Meteor.user()._id}, {$set:{
             "profile.name":username,
             "profile.lastname":lastname
---
+++ server/templates/profile/profile.js
@@ -0,0 +1,3 @@
+Meteor.methods({
+    updateemailfunction: function (email) { Meteor.users.update({_id:Meteor.user()._id}, {$set: {'emails.0.address': email}}); }
+});
